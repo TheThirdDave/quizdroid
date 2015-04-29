a1e266c3d225efea63a708d1a7ed87e640fc9170
David <uski2011@gmail.com>
2015-04-29 03:23:59 -0700

    added topicactivity class for the topic activity and added button listeners for clicking on topics in the main activity

--- app/src/main/java/phida11/washington/edu/quizdroid/MainActivity.java
+++ app/src/main/java/phida11/washington/edu/quizdroid/MainActivity.java
@@ -1,17 +1,48 @@
 package phida11.washington.edu.quizdroid;
 
+import android.app.Activity;
+import android.content.DialogInterface;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.Switch;
 
 
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends Activity implements OnClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button button1 = (Button) findViewById(R.id.button1);
+        button1.setOnClickListener(this);
+        Button button2 = (Button) findViewById(R.id.button2);
+        button2.setOnClickListener(this);
+        Button button3 = (Button) findViewById(R.id.button3);
+        button3.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+
+            case R.id.button1:
+
+                break;
+
+            case R.id.button2:
+
+                break;
+
+            case R.id.button3:
+
+                break;
+        }
     }
 
 
