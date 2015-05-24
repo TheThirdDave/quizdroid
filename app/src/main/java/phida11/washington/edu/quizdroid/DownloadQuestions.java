469970ae9c738dcdc9692b54cc5ea09e098403ae
David <uski2011@gmail.com>
2015-05-23 23:08:48 -0700

    started building IntentService for background work to download the JSON file. For this part it will just be displaying a Toast message with the URL provided by the user

---
+++ app/src/main/java/phida11/washington/edu/quizdroid/DownloadQuestions.java
@@ -0,0 +1,27 @@
+package phida11.washington.edu.quizdroid;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.content.Context;
+
+/**
+ * An {@link IntentService} subclass for handling asynchronous task requests in
+ * a service on a separate handler thread.
+ * <p/>
+ * TODO: Customize class - update intent actions, extra parameters and static
+ * helper methods.
+ */
+public class DownloadQuestions extends IntentService {
+
+
+    public DownloadQuestions() {
+        super("DownloadQuestions");
+    }
+
+    @Override
+    protected void onHandleIntent(Intent intent) {
+        if (intent != null) {
+            final String action = intent.getAction();
+        }
+    }
+}
